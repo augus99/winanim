d2b76e9435f45b67d6c3c36967675ac841015280
augus99 <augustocastelo1999@gmail.com>
2021-02-17 16:45:43 -0300

    Add basic form setup

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,10 +1,24 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Winanim {
     /// <summary>
     /// Main window that contains all buttons
     /// </summary>
     public class MainWindow: Form {
-        
+        public MainWindow() {
+            this.SuspendLayout();
+
+            // Setup form
+            this.AutoScaleDimensions = new SizeF(6F, 13F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(287, 405);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = Color.FromArgb(31, 31, 31);
+            this.Name = "AnimateWindowForm";
+            this.Text = "Animated Window";
+
+            this.ResumeLayout(false);
+        }
     }
 }
